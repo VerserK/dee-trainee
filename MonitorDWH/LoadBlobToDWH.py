e1a44c984e3df833aa1c6f8650aa906e4d68866e
Nongnaphat Wongpiyachai <nongnaphat.wongpiyachai@gmail.com>
2023-07-07 08:49:11 +0700

    fix yest_date

--- MonitorDWH/LoadBlobToDWH.py
+++ MonitorDWH/LoadBlobToDWH.py
@@ -15,7 +15,8 @@ duration = timedelta(days = 1, hours = 0, minutes = 0)
 # convert string date to a datetime object
 current_date = datetime.strptime(current_date, "%Y%m%d")
 
-yest_date = (current_date - duration).strftime("y=%Y/m=%m/d=%d")
+# yest_date = (current_date - duration).strftime("y=%Y/m=%m/d=%d")
+yest_date = 'y=2023/m=07/d=06'
 print(yest_date)
 
 # enter credentials
