efd52c154b10e94b00244117babc5b6d633ca5f8
Nongnaphat Wongpiyachai <nongnaphat.wongpiyachai@gmail.com>
2023-08-15 09:19:53 +0700

    test

--- MonitorDWH/LoadBlobToDWH.py
+++ MonitorDWH/LoadBlobToDWH.py
@@ -26,9 +26,7 @@ blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 container_client = blob_service_client.get_container_client(container_name)
 
 def run():
-    # current_date = datetime.now()
-    duration = timedelta(days = 1)
-    current_date = datetime.now() - duration
+    current_date = datetime.now()
     logging.info(current_date)
 
     one_day = timedelta(days = 1, hours = 0, minutes = 0)
