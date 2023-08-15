4a69a3c4a0abc51442cf0ae3e0b860f5e357a255
Nongnaphat Wongpiyachai <nongnaphat.wongpiyachai@gmail.com>
2023-08-15 09:00:41 +0700

    fixed data on date 2023-08-12

--- MonitorDWH/LoadBlobToDWH.py
+++ MonitorDWH/LoadBlobToDWH.py
@@ -27,7 +27,7 @@ container_client = blob_service_client.get_container_client(container_name)
 
 def run():
     # current_date = datetime.now()
-    duration = timedelta(days = 4)
+    duration = timedelta(days = 3)
     current_date = datetime.now() - duration
     logging.info(current_date)
 
